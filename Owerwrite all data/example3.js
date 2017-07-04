567ccac93d10164ee6a97a31de8b5541dabcb2bd
Luca Vercelli <luca.vercelli.to@gmail.com>
2017-07-04 08:00:23 +0200

    example3

---
+++ Owerwrite all data/example3.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+
+  var columnDefs = [{
+    title: "Id",
+    type: "readonly"
+  }, {
+    title: "Name"
+  },
+ {
+    data: "position",
+    title: "Position"
+  },
+ {
+    data: "office",
+    title: "Office"
+  },
+ {
+    data: "extension",
+    title: "Extn."
+  },
+ {
+    data: "startDate",
+    title: "Start date"
+  },
+ {
+    data: "salary",
+    title: "Salary"
+  }];
+
+  var myTable;
+
+  myTable = $('#example').DataTable({
+    "sPaginationType": "full_numbers",
+    ajax: './data3.json',
+    columns: columnDefs,
+		dom: 'Bfrtip',        // Needs button container
+          select: 'single',
+          responsive: true,
+          altEditor: true,     // Enable altEditor
+          buttons: [{
+            text: 'Add',
+            name: 'add'        // do not change name
+          },
+
+          {
+            extend: 'selected', // Bind to Selected row
+            text: 'Edit',
+            name: 'edit'        // do not change name
+          },
+
+          {
+            extend: 'selected', // Bind to Selected row
+            text: 'Delete',
+            name: 'delete'      // do not change name
+         }]
+  });
+
+
+});
+
