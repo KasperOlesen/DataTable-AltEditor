3e338feccb55719746a30fd396a0646db501fbcc
Luca Vercelli <luca.vercelli.to@gmail.com>
2019-12-02 09:33:37 +0100

    translations

--- example/09_translations/example9.js
+++ example/09_translations/example9.js
@@ -73,41 +73,66 @@ $(document).ready(function() {
           responsive: true,
           altEditor: true,     // Enable altEditor
           buttons: [{
-            text: 'Add',
+            text: 'Aggiungi',
             name: 'add'        // do not change name
           },
           {
             extend: 'selected', // Bind to Selected row
-            text: 'Edit',
+            text: 'Modifica',
             name: 'edit'        // do not change name
           },
           {
             extend: 'selected', // Bind to Selected row
-            text: 'Delete',
+            text: 'Elimina',
             name: 'delete'      // do not change name
          }],
     language: {
-        altEditor : {
-            modalClose : "Chiudi",
-            edit : {
-                title : "Modifica",
-                button : "Modifica"
+        
+        // see https://datatables.net/plug-ins/i18n/Italian
+        "sEmptyTable":     "Nessun dato presente nella tabella",
+        "sInfo":           "Vista da _START_ a _END_ di _TOTAL_ elementi",
+        "sInfoEmpty":      "Vista da 0 a 0 di 0 elementi",
+        "sInfoFiltered":   "(filtrati da _MAX_ elementi totali)",
+        "sInfoPostFix":    "",
+        "sInfoThousands":  ".",
+        "sLengthMenu":     "Visualizza _MENU_ elementi",
+        "sLoadingRecords": "Caricamento...",
+        "sProcessing":     "Elaborazione...",
+        "sSearch":         "Cerca:",
+        "sZeroRecords":    "La ricerca non ha portato alcun risultato.",
+        "oPaginate": {
+            "sFirst":      "Inizio",
+            "sPrevious":   "Precedente",
+            "sNext":       "Successivo",
+            "sLast":       "Fine"
+        },
+        "oAria": {
+            "sSortAscending":  ": attiva per ordinare la colonna in ordine crescente",
+            "sSortDescending": ": attiva per ordinare la colonna in ordine decrescente"
+        },
+        
+        // see /translations
+        "altEditor" : {
+            "modalClose" : "Chiudi",
+            "edit" : {
+                "title" : "Modifica",
+                "button" : "Modifica"
             },
-            add : {
-                title : "Nuovo",
-                button : "Crea"
+            "add" : {
+                "title" : "Nuovo",
+                "button" : "Crea"
             },
-            delete : {
-                title : "Elimina",
-                button : "Elimina"
+            "delete" : {
+                "title" : "Elimina",
+                "button" : "Elimina"
             },
-            success : "Fatto.",
-            error : {
-                message : "Si è verificato un errore non previsto.",
-                label : "Errore",
-                responseCode : "Response code:",
-                required : "Valore obbligatorio",
-                unique : "Valore duplicato"
+            "success" : "Fatto.",
+            "error" : {
+                "message" : "Si è verificato un errore non previsto.",
+                "label" : "Errore",
+                "responseCode" : "Response code:",
+                "required" : "Valore obbligatorio",
+                "unique" : "Valore duplicato"
             }
         }
     }
